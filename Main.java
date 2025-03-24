b1310835307ab168e2cc74a207fb035906676b79
Rob Schlenker <rschlenker@onemagnify.com>
2025-03-24 10:15:30 -0400

    testing with numbers

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("I am learning Java.");
-        System.out.println("It is awesome!");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
     }
   }
