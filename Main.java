3d816d77e316dc05be322009b66ecfd2712956ba
Rob Schlenker <rschlenker@onemagnify.com>
2025-03-25 13:41:31 -0400

    more practice

--- Main.java
+++ Main.java
@@ -1,18 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Student data
-        String studentName = "John Doe";
-        int studentID = 15;
-        int studentAge = 23;
-        float studentFee = 75.25f;
-        char studentGrade = 'B';
-
-        // Print variables
-        System.out.println("Student name: " + studentName);
-        System.out.println("Student id: " + studentID);
-        System.out.println("Student age: " + studentAge);
-        System.out.println("Student fee: " + studentFee);
-        System.out.println("Student grade: " + studentGrade);
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
     }
 }
